7a4221255205685347d27c36d64e1b4bbd84d6df
Overlander <anuragnandal@hotmail.com>
2022-12-01 14:46:53 -0500

    Let Resume component render General component

---
+++ src/components/Resume.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import General from "./General";
+
+class Resume extends Component {
+  render() {
+    return(
+      <div className="resume">
+        <General />
+      </div>
+    )
+  }
+}
+
+export default Resume;
