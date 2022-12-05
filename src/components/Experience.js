e048f43e323b57b7352c241748c1983eea82e936
Overlander <anuragnandal@hotmail.com>
2022-12-05 15:35:26 -0500

    Change class component to function component with hooks

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,128 +1,88 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import uniqid from "uniqid";
 
-class Experience extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      jobs: [],
-      job: {
-        id: uniqid(),
-        company: '',
-        position: '',
-        description: '',
-        startDate: '',
-        endDate: '',
-        experienceFormActive: true,
-        experienceDivActive: false
-      }
+const Experience = () => {
+  const [jobs, setJobs] = useState([]);
+  const [job, setJob] = useState(
+    {
+      id: uniqid(),
+      company: '',
+      position: '',
+      description: '',
+      startDate: '',
+      endDate: '',
+      experienceFormActive: true,
+      experienceDivActive: false
     }
-  }
+  );
 
-  handleChange = (event) => {
+  const handleChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
-    this.setState({
-      job: { ...this.state.job, [name]: value }
-    });
+    setJob({ ...job, [name]: value });
   }
 
-  toggleForm = () => {
-    this.setState({ experienceFormActive: !this.state.experienceFormActive });
+  const toggleForm = () => {
+    setJob({ ...job, experienceFormActive: !job.experienceFormActive });
   }
 
-  submitForm = (e) => {
+  const submitForm = (e) => {
     e.preventDefault();
-    this.setState({
-      jobs: this.state.jobs.concat(this.state.job),
-      job: { ...this.state.job, id: uniqid() }
-    });
-    this.toggleForm();
+    setJobs(jobs.concat(job));
+    setJob({ ...job, id: uniqid() });
+    toggleForm();
   }
 
-  handleDelete = (id) => {
-    this.setState({ jobs: this.state.jobs.filter(job => job.id !== id) });
-  }
+  const handleDelete = (id) => setJobs(jobs.filter(job => job.id !== id));
 
-  render() {
-    return(
-      <div className="experience">
-        <h4>Experience</h4>
+  return(
+    <div className="experience">
+      <h4>Experience</h4>
 
-        <div>
-          {this.state.jobs.map(job => {
-            return <div key={job.id} className="experience-info">
-                    <div>{job.company}</div>
-                    <div>{job.position}</div>
-                    <div>{job.description}</div>
-                    <div>From: {job.startDate}</div>
-                    <div>To: {job.endDate}</div>
-                    <button onClick={this.handleDelete.bind(this, job.id)}>Remove</button>
-                   </div>;
-          })}
-        </div>
-
-        <button onClick={this.toggleForm}>Add Experience</button>
-
-        {this.state.experienceFormActive ?
-          <form onSubmit={this.submitForm}>
-            <div className="experience-form">
-              <div>
-                <label htmlFor="company">Company</label>
-                <input
-                  onChange={this.handleChange}
-                  type="text"
-                  id="company"
-                  name="company"
-                  value={this.state.job.company}
-                />
-                <label htmlFor="position">Position</label>
-                <input
-                  onChange={this.handleChange}
-                  type="text"
-                  id="position"
-                  name="position"
-                  value={this.state.job.position}
-                />
-              </div>
-              <div>
-                <label htmlFor="description">Description</label>
-                <textarea
-                  onChange={this.handleChange}
-                  rows="4"
-                  cols="50"
-                  id="description"
-                  name="description"
-                  value={this.state.job.description}
-                />
-              </div>
-              <div>
-                <label htmlFor="startDate">Start</label>
-                <input
-                  onChange={this.handleChange}
-                  type="date"
-                  id="startDate"
-                  name="startDate"
-                  value={this.state.job.startDate}
-                />
-                <label htmlFor="endDate">End</label>
-                <input
-                  onChange={this.handleChange}
-                  type="date"
-                  id="endDate"
-                  name="endDate"
-                  value={this.state.job.endDate}
-                />
-              </div>
-            </div>
-            <button type="submit" className="submit">Submit</button>
-            <button onClick={this.toggleForm}>Cancel</button>
-          </form>
-        : null}
+      <div>
+        test
+        {jobs.map(job => {
+          return <div key={job.id} className="experience-info">
+            {console.log(job)}
+                  <div>{job.company}</div>
+                  <div>{job.position}</div>
+                  <div>{job.description}</div>
+                  <div>From: {job.startDate}</div>
+                  <div>To: {job.endDate}</div>
+                  <button onClick={handleDelete(job.id)}>Remove</button>
+                  </div>;
+        })}
       </div>
-    )
-  }
+
+      <button onClick={toggleForm}>Add Experience</button>
+
+      {job.experienceFormActive ?
+        <form onSubmit={submitForm}>
+          <div className="experience-form">
+            <div>
+              <label htmlFor="company">Company</label>
+              <input onChange={handleChange} type="text" id="company" name="company" value={job.company}/>
+              <label htmlFor="position">Position</label>
+              <input onChange={handleChange} type="text" id="position" name="position" value={job.position}/>
+            </div>
+            <div>
+              <label htmlFor="description">Description</label>
+              <textarea onChange={handleChange} rows="4" cols="50" id="description" name="description" value={job.description}/>
+            </div>
+            <div>
+              <label htmlFor="startDate">Start</label>
+              <input onChange={handleChange} type="date" id="startDate" name="startDate" value={job.startDate}/>
+              <label htmlFor="endDate">End</label>
+              <input
+                onChange={handleChange} type="date" id="endDate" name="endDate" value={job.endDate}/>
+            </div>
+          </div>
+          <button type="submit" className="submit">Submit</button>
+          <button onClick={toggleForm}>Cancel</button>
+        </form>
+      : null}
+    </div>
+  )
 }
 
 export default Experience;
